e4296630706d61823845573ac3280ea65ba2f605
Gustavo <gualeixos456@gmail.com>
2024-11-24 21:10:43 -0300

    Add files via upload

--- backend/nodejs/server.js
+++ backend/nodejs/server.js
@@ -1,11 +1,50 @@
-import express from "express";
+const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
 
 const app = express();
+const PORT = 3000;
 
-app.listen(3000, () => {
-  console.log("Servidor inicializado!");
+mongoose.connect('mongodb+srv://gualeixos456:Dk3A8PfDnK6dCAIr@gustavo.no5ff.mongodb.net/?retryWrites=true&w=majority&appName=Gustavo', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB conectado!'))
+  .catch(err => console.error(err));
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Rota de Registro
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = new User({ username, password });
+    await user.save();
+    res.status(201).send('Usuário registrado com sucesso!');
+  } catch (err) {
+    res.status(400).send('Erro ao registrar usuário.');
+  }
 });
 
-app.get("/", (req, res) => {
-  res.status(200).send("Mensagem do Node.js");
+// Rota de Login
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) return res.status(404).send('Usuário não encontrado.');
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).send('Senha incorreta.');
+
+    const token = jwt.sign({ id: user._id }, 'secrect_key', { expiresIn: '1h' });
+    res.status(200).json({ token });
+  } catch (err) {
+    res.status(500).send('Erro no login.');
+  }
 });
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
