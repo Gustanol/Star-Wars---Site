9f7aba0dd57dce200ca53036f1b709d1b4195152
Gustavo <gualeixos456@gmail.com>
2024-11-25 00:21:00 -0300

    Add files via upload

--- backend/nodejs/server.js
+++ backend/nodejs/server.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const User = require('./models/User');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Servidor está funcionando!');
+});
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
