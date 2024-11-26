b49a3f46d79c938884901c8fbf3e188c8e52c88c
Gustavo <gualeixos456@gmail.com>
2024-11-26 00:20:07 -0300

    Add files via upload

--- frontend/src/SW.js
+++ frontend/src/SW.js
@@ -317,3 +317,70 @@ document.addEventListener("DOMContentLoaded", function () {
        });
     });
 });
+
+const form1 = document.getElementById('loginForm');
+const form2 = document.getElementById('registerForm');
+const part1 = document.querySelector('.class2');
+const class2T = document.getElementById('class2T');
+        document.getElementById('register').addEventListener('click', () => {
+            part1.classList.add('animation-toLeft');
+            setTimeout(() => {
+                form1.style.display = 'none';
+                part1.classList.remove('animation-toLeft');
+                part1.style.right = '50vw';                
+            }, 700);
+            form2.style.display = 'flex';
+            class2T.innerHTML='Registro';
+        });
+        document.getElementById('login').addEventListener('click', () => {
+            part1.classList.add('animation-toRight');
+            setTimeout(() => {
+                form2.style.display = 'none';
+                part1.classList.remove('animation-toRight');
+                part1.style.right = '0';
+            }, 700);
+            form1.style.display = 'flex';
+            class2T.innerHTML='Login'; 
+        });
+        
+        const API_URL = 'https://star-wars-site.onrender.com';
+        
+        // Registrar usuário
+document.getElementById('registerForm').addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData.entries());
+
+  try {
+    const response = await fetch(`${API_URL}/signup`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.text();
+    alert(result);
+  } catch (error) {
+    console.error('Erro:', error);
+  }
+});
+
+// Login do usuário
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData.entries());
+
+  try {
+    const response = await fetch(`${API_URL}/login`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.text();
+    alert(result);
+  } catch (error) {
+    console.error('Erro:', error);
+  }
+});
