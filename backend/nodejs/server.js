508a5e43eb58ecb2e64effa3087d66d00cd1ad42
Gustavo <gualeixos456@gmail.com>
2024-12-02 23:53:53 -0300

    Add files via upload

--- backend/nodejs/server.js
+++ backend/nodejs/server.js
@@ -4,10 +4,6 @@ const bodyParser = require('body-parser');
 const User = require('./models/User');
 const cors = require('cors');
 const https = require('https');
-const ejs = require('ejs');
-const fs = require('fs');
-const path = require('path');
-const { exec } = require('child_process');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -71,62 +67,6 @@ setInterval(() => {
     });
 }, 300000); // 1000 ms = 1s
 
-// Diretório de salvamento dos arquivos HTML
-const outputDir = path.join(__dirname, 'generated');
-
-// Certificando que ele existe
-if (!fs.existsSync(outputDir)) {
-  fs.mkdirSync(outputDir, { recursive: true });
-}
-
-const pages = [];
-
-app.post('/org', async (req, res) => {
-  const { title, orgType, chief, capital, coin, followers } = req.body;
-  // Criar um objeto com os dados recebidos
-  const newPage = {
-    title,
-    orgType,
-    chief,
-    capital,
-    coin,
-    followers
-  };
-  // Adicionar o objeto à lista
-  pages.push(newPage);
-
-  pages.forEach((page, index) => {
-  ejs.renderFile(path.join(__dirname, 'views', 'org.ejs'), page, (err, html) => {
-    if (err) {
-      console.error(`Erro ao renderizar página ${index + 1}:`, err);
-      return;
-    }
-
-    const filePath = path.join(outputDir, `page${index + 1}.html`);
-    fs.writeFileSync(filePath, html);
-    console.log(`Página salva em: ${filePath}`);
-  });
-});
-});
-
-const repoPath = 'https://github.com/Gustanol/Star-Wars---Site/blob/main';
-
-exec(
-  `
-  cd ${repoPath} &&
-  git add frontend/src/html/* &&
-  git commit -m "Adicionando arquivos HTML" &&
-  git push
-  `,
-  (err, stdout, stderr) => {
-    if (err) {
-      console.error(`Erro: ${stderr}`);
-      return;
-    }
-    console.log('Arquivos enviados ao GitHub com sucesso!');
-  }
-);
-  
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
